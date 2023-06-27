7ef52bb6817f5a2e7213bf09acb5c52959f7bf63
ameya991 <127427455+ameya991@users.noreply.github.com>
2023-06-27 18:54:59 +0530

    Delete Polling-system-api-main directory

--- Polling-system-api-main/controllers/home_controller.js
+++
@@ -1,7 +0,0 @@
-
-exports.home = (req, res) => {
-    res.status(200).json({
-        success: true,
-        greeting: 'Hello from the polling system api'
-    })
-}
--- Polling-system-api-main/routes/options.js
+++
@@ -1,9 +0,0 @@
-const express = require('express')
-const router = express.Router()
-
-const optionsController = require('../controllers/options_controller')
-
-router.delete('/:id/delete', optionsController.deleteOption)
-router.put('/:id/add_vote', optionsController.addVote)
-
-module.exports = router
